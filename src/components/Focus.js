11b02a5ed87dfcfd27b913149fee8b4c49d74083
Joshua Freedman <jf248@users.noreply.github.com>
2018-01-14 00:17:52 -0800

    change Focus to a class

--- src/components/Focus.js
+++ src/components/Focus.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
 import State from './State'
 import renderProps from '../utils/renderProps'
 
-const Focus = ({ onChange, ...props }) => (
-  <State initial={{ focused: false }} onChange={ onChange }>
-    {({ state, setState }) => renderProps(props, {
-      isFocus: state.focused,
-      focused: state.focused,
-      bindFocus: {
-        onFocus: () => setState({ focused: true }),
-        onBlur: () => setState({ focused: false }),
-      },
-    })}
-  </State>
-)
+class Focus extends Component {
+  render() {
+    const { onChange, ...props } = this.props
+    return (
+      <State initial={{ focused: false }} onChange={ onChange }>
+        {({ state, setState }) => renderProps(props, {
+          isFocus: state.focused,
+          focused: state.focused,
+          bindFocus: {
+            onFocus: () => setState({ focused: true }),
+            onBlur: () => setState({ focused: false }),
+          },
+        })}
+      </State>
+    )
+  }
+}
 
 export default Focus
